0d2e6541bfc7dad049bd166999b3fcf22e06e050
vivz-git <176604716+vivz-git@users.noreply.github.com>
2026-03-31 15:46:20 +0530

    Fixed structure: removed venv and moved project files to root

--- .venv/fields.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel, Field 
-
-class Student(BaseModel):
-       name: str = Field(min_length=3)
-       age : int = Field(gt=0)
--- .venv/myenv/Scripts/basic.sql
+++
@@ -1,2 +0,0 @@
-CREATE DATABASE vivzz_db;
-
